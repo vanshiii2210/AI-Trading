c88bffb55d625ee7a2eaa47350786bfa8d1bc1dd
vanshika <141492120+vanshiii2210@users.noreply.github.com>
2026-06-12 13:13:06 +0530

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-import path from "path";
-
-const nextConfig: NextConfig = {
-  // Ensure Next finds src/app even in monorepo / Vercel edge cases
-  outputFileTracingRoot: path.join(__dirname),
-};
-
-export default nextConfig;
